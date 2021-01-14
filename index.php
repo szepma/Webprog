1a7a23e37701cdbd4783ae3b8cca21e8ed29cc21
szepma <61704274+szepma@users.noreply.github.com>
2021-01-14 16:13:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+include "inc/functions.php";
+
+include "pages/begin.php";
+
+$page = "home";
+
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+}
+
+$file = "pages/page-". $page .".php";
+
+if (is_file($file)) {
+    include $file;
+}
+else {
+    echo '<h1>Nincs ilyen</h1>';
+    echo '<p>A kért lap nem található</p>';
+}
+
+include "pages/end.php";
+
+?>
